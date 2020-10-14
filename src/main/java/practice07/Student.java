e45275d3ed3063050e3c7c0f000f5901a81adb27
Erik <erik.daniel.nobleta@oocl.com>
2020-10-14 18:08:20 +0800

    [Practice07][Erik - 1] feature - made all tests pass and modified classes inheritance

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,33 @@
 package practice07;
 
-public class Student {
+public class Student extends Person{
+
+    private Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old. I am a Student. I am at Class " + klass.getNumber() + ".";
+    }
 }
